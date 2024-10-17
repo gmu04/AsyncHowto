d8ab152d585e21016dd9b1e54b2a851ec988bf4f
Gokhan Mutlu <gokhanmutlu04@yahoo.com>
2024-10-17 05:46:48 +0300

    Initial Commit

---
+++ AsynchHowto/AsynchHowtoApp.swift
@@ -0,0 +1,12 @@
+// AsynchHowtoApp.swift by Gokhan Mutlu on 17.10.2024
+
+import SwiftUI
+
+@main
+struct AsynchHowtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
